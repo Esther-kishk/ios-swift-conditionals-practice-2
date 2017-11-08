06b133e09aaa22ed7bd0a7a6b9206bfe5a95ace0
Esther Kishk <esther.kishk@MDYHSs-iMac-39.local>
2017-11-08 15:04:30 -0500

    donee

--- ConditionalsPractice2.playground/Contents.swift
+++ ConditionalsPractice2.playground/Contents.swift
@@ -6,33 +6,34 @@ import UIKit
 //Try to use a different logical operator each time
 //after you type in the operator, delete the comment to test out the statement
 
-//0 > -4 ____ 100 == 100
+0 > -4 && 100 == 100
 
-//45 != 46 ____ 5 > 3
+45 != 46 && 5 > 3
 
-//__(8 > 10) ____ 10 == 11
+!(8 > 10) && 10 != 11
 
 
 //Replace the '__' with logical operators to make the following statements false
 //Try to use a different logical operator each time
 //after you type in the operator, delete the comment to test out the statement
 
-//2 == -4 ____ 100 == 100
+2 > -4 && 100 == 100
 
-//-55 != -55 ____ 5 <= 3
+-55 == -55 || 5 <= 3
 
-//15 > 10 ____ !(30 == 30)
+15 != 10  && (30 == 30)
 
 
 //-----------------------------------------------------------------------------------
 
 
 //replace the blank with two conditional statements connected with a logical operator that will determine if the person is a teenager.
-var age: Int // = ______
+var age: Int = 14
 
-//if ________  ___  ________ {
-//    print("You are a teenager!")
-//}
+if (age > 12)  &&  (age < 20) {
+    print("You are a teenager!")
+}
+
 
 
 
@@ -40,16 +41,16 @@ var age: Int // = ______
 
 
 //replace the blank with two conditional statements connected with a logical operator that prints the correct statement based on whether or not today is a weekday and whether or not there is a lot of traffic today.
-var isWeekday = true
-var isTraffic = true
+var isWeekday = false
+var isTraffic = false
 
-//if ________  ___  ________ {
-//    print("There is a lot of traffic today, you should probably take the train.")
-//} else if ________  ___  ________ {
-//    print("You should drive to work today, there is no traffic and you'll make it there much faster")
-//} else {
-//    print("It's the weekend! No work today.")
-//}
+if isWeekday && isTraffic == true {
+    print("There is a lot of traffic today, you should probably take the train.")
+} else if  isWeekday && isTraffic != true {
+    print("You should drive to work today, there is no traffic and you'll make it there much faster")
+} else {
+    print("It's the weekend! No work today.")
+}
 
 
 
@@ -57,17 +58,17 @@ var isTraffic = true
 
 
 //replace the blanks with conditional statements linked with logical operators when appropriate so that the appropriate message is printed. Hint: the % operator might be useful here.
-var number: Int // = ______
+var number: Int  = 15
 
-//if __________ {
-//    print("That number is divisible by 5 and 3")
-//} else if __________{
-//    print("That number is only divisible by 5")
-//} else if ___________ {
-//    print("That number is only divisible by 3")
-//} else {
-//    print("That number isn't divisible by 5 or 3.")
-//}
+if ((number % 3 = 0) && (number % 5 = 0)){
+print("That number is divisible by 5 and 3")
+} else if ((number % 5 = 0) && (number % != 3 )){
+    print("That number is only divisible by 5")
+} else if ((number % 3 = 0) && (number % 5 != 0)){
+    print("That number is only divisible by 3")
+} else  ((number % 3 != 0) && (number % 5 != 0)){
+   print("That number isn't divisible by 5 or 3.")
+}
 
 
 
